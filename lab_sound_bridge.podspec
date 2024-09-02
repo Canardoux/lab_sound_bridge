016ddf49af226156912476dab9da9bf23f60ee0d
larpoux <larpoux@gmail.com>
2024-09-02 10:14:08 +0200

    lab_sound-bridge : Version 0.0.4

--- lab_sound_bridge.podspec
+++ lab_sound_bridge.podspec
@@ -1,15 +1,20 @@
 Pod::Spec.new do |s|
     s.name         = "lab_sound_bridge"
-    s.version      = "0.0.3"
-    s.summary      = "VeryUsefulFramework: VeryUsefulFramework"
-    s.description  = "your description. Vraiment tres utile ce plugin"
+    s.version      = '0.0.4'
+    s.summary      = "Bridge to LabSound for Flutter"
+    s.description      = <<-DESC
+    This lib is used by the flutter plugin `lab_sound_flutter` to access the LabSound lib.
+    It has been extracted to be isolated from Flutter and can be used with other frameworks 
+    (for example pure dart).
+                           DESC
     s.homepage     = "https://github.com/canardoux/lab_sound_bridge.git"
     s.license = { :type => "BSD", :file => "LICENSE" }
-    s.author             = { "Oguzhan Karakus" => "your@mail.com" }
+    s.author             = { "Xioxin" => "your@mail.com" }
     s.source       = { :git => "https://github.com/canardoux/lab_sound_bridge.git", :branch => "main", :tag => "#{s.version}" }
-    s.vendored_frameworks = "build-xcframework/LabSoundBridge.xcframework/ios-arm64/LabSoundBridge.framework"
+    s.vendored_frameworks = "ios/build-xcframework/LabSoundBridge.xcframework"
     s.platform = :ios
     s.swift_version = "5.7"
-    s.ios.deployment_target  = '16.0'
+    s.ios.deployment_target  = '12.0'
+    s.source_files = 'bridge/*'
     s.requires_arc = true
 end
--- tools/build_ios.sh
+++ tools/build_ios.sh
@@ -1,51 +1,58 @@
 #!/bin/bash
 
-echo '--- build'
-echo '---------'
-rm -rf build
-mkdir build
-cmake -B ./build -G "Xcode" -DPLATFORM=OS64 -DCMAKE_TOOLCHAIN_FILE=../cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build ./build --config Release
+echo ''
+echo '-------------'
+echo '--- build ---'
+echo '-------------'
+rm -rf ios/build
+mkdir ios/build
+cmake -B ./ios/build -G "Xcode" -DPLATFORM=OS64 -DCMAKE_TOOLCHAIN_FILE=cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build ./ios/build --config Release
 if [ $? -ne 0 ]; then
-    echo "Error: cmake --build ./build --config Release"
+    echo "Error: cmake --build ./ios/build --config Release"
     exit -1
 fi
 
-echo '--- build-simulator'
-echo '-------------------'
-rm -rf build-simulator
-mkdir build-simulator
-cmake -B ./build-simulator -G "Xcode" -DPLATFORM=SIMULATOR64 -DCMAKE_TOOLCHAIN_FILE=../cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build ./build-simulator --config Release
+echo ''
+echo '-----------------------'
+echo '--- build-simulator ---'
+echo '-----------------------'
+rm -rf ios/build-simulator
+mkdir ios/build-simulator
+cmake -B ./ios/build-simulator -G "Xcode" -DPLATFORM=SIMULATOR64 -DCMAKE_TOOLCHAIN_FILE=cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build ./ios/build-simulator --config Release
 if [ $? -ne 0 ]; then
-    echo "Error: cmake --build ./build-simulator --config Release"
-    exit -1
-fi
-
-
-echo '--- build-simulator-arm64'
-echo '-------------------------'
-rm -rf  build-simulator-arm64
-mkdir build-simulator-arm64
-cmake -B ./build-simulator-arm64 -G "Xcode" -DPLATFORM=SIMULATORARM64 -DCMAKE_TOOLCHAIN_FILE=../cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build ./build-simulator-arm64 --config Release
-if [ $? -ne 0 ]; then
-    echo "Error: cmake --build ./build-simulator --config Release"
+    echo "Error: cmake --build ./ios/build-simulator --config Release"
     exit -1
 fi
 
 
+echo ''
+echo '-----------------------------'
+echo '--- build-simulator-arm64 ---'
+echo '-----------------------------'
+rm -rf  ios/build-simulator-arm64
+mkdir ios/build-simulator-arm64
+cmake -B ./ios/build-simulator-arm64 -G "Xcode" -DPLATFORM=SIMULATORARM64 -DCMAKE_TOOLCHAIN_FILE=cmake/ios-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build ./ios/build-simulator-arm64 --config Release
+if [ $? -ne 0 ]; then
+    echo "Error: cmake --build ./ios/build-simulator-arm64 --config Release"
+    exit -1
+fi
 
-echo '--- build-xcframework'
-echo '---------------------' 
-rm -rf build-xcframework
-mkdir -p build-xcframework
+
+echo ''
+echo '-------------------------' 
+echo '--- build-xcframework ---'
+echo '-------------------------' 
+rm -rf ios/build-xcframework
+mkdir -p ios/build-xcframework
 xcodebuild -create-xcframework \
-    -framework ./build-simulator-arm64/Release-iphonesimulator/LabSoundBridge.framework\
-    -framework ./build/Release-iphoneos/LabSoundBridge.framework \
-    -output "build-xcframework/LabSoundBridge.xcframework"
+    -framework ./ios/build-simulator-arm64/Release-iphonesimulator/LabSoundBridge.framework\
+    -framework ./ios/build/Release-iphoneos/LabSoundBridge.framework \
+    -output "ios/build-xcframework/LabSoundBridge.xcframework"
 if [ $? -ne 0 ]; then
-    echo "Error: cmake --build ./build-simulator --config Release"
+    echo "Error: cmake --build xcodebuild -create-xcframework  --config Release"
     exit -1
 fi
 
@@ -64,36 +71,41 @@ fi
 #    exit -1
 #fi
 
-echo '--- lipo'
-echo '--------'
-rm -rf ./products
-mkdir -p ./products/LabSoundBridge.framework
-lipo -create -output ./products/LabSoundBridge.framework/LabSoundBridge ./build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator/LabSoundBridge.framework/LabSoundBridge ./build-simulator/Release-iphonesimulator/LabSoundBridge.framework/LabSoundBridge 
+echo ''
+echo '------------'
+echo '--- lipo ---'
+echo '------------'
+rm -rf ./ios/products
+mkdir -p ./ios/products/LabSoundBridge.framework
+lipo -create -output ./ios/products/LabSoundBridge.framework/LabSoundBridge ./ios/build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator/LabSoundBridge.framework/LabSoundBridge ./ios/build-simulator/Release-iphonesimulator/LabSoundBridge.framework/LabSoundBridge 
 if [ $? -ne 0 ]; then
-    echo "Error: lipo -create -output ./products/LabSoundBridge.framework/LabSoundBridge ./build/Release-iphoneos/LabSoundBridge.framework/LabSoundBridge ./build-simulator-arm64/LabSoundBridge.framework/LabSoundBridge"
+    echo "Error: lipo -create -output ./ios/products/LabSoundBridge.framework/LabSoundBridge ./ios/build/Release-iphoneos/LabSoundBridge.framework/LabSoundBridge ./ios/build-simulator-arm64/LabSoundBridge.framework/LabSoundBridge"
     #exit -1
 fi
-#cp ./products/LabSoundBridge.framework/LabSoundBridge ./build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator/LabSoundBridge.framework/
+####cp ./products/LabSoundBridge.framework/LabSoundBridge ./build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator/LabSoundBridge.framework/
 
-echo '--- codesign'
-echo '------------'
+echo ''
+echo '----------------'
+echo '--- codesign ---'
+echo '----------------'
 security create-keychain -p abc123 build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p abc123 build.keychain
 security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k abc123 build.keychain
 codesignIdentity=`security find-identity -p codesigning -v | grep -Eo "[0-9A-F]{40}" | head -n 1`
-/usr/bin/codesign --force -s $codesignIdentity ./build-xcframework/LabSoundBridge.xcframework/ios-arm64/LabSoundBridge.framework -v
+/usr/bin/codesign --force -s $codesignIdentity ./ios/build-xcframework/LabSoundBridge.xcframework/ios-arm64/LabSoundBridge.framework -v
+/usr/bin/codesign --force -s $codesignIdentity ./ios/build-xcframework/LabSoundBridge.xcframework -v
 if [ $? -ne 0 ]; then
-    echo "Error: /usr/bin/codesign --force -s $codesignIdentity ./products/LabSoundBridge.framework -v"
+    echo "Error: /usr/bin/codesign --force -s $codesignIdentity ./ios/build-xcframework/LabSoundBridge.xcframework/ios-arm64/LabSoundBridge.framework -v"
     exit -1
 fi
-rm -rf LabSoundBridge.xcframework/ios-arm64-simulator
-cp -a products/LabSoundBridge.framework build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator
+# rm -rf LabSoundBridge.xcframework/ios-arm64-simulator
+# cp -a products/LabSoundBridge.framework build-xcframework/LabSoundBridge.xcframework/ios-arm64-simulator
 
 
-echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+echo $MACOS_CERTIFICATE | base64 --decode > ios/certificate.p12
 
 
-tar -zcvf LabSoundBridge_ios.tar.gz -C ./build-xcframework LabSoundBridge.xcframework
-echo "E.O.J"
+tar -zcvf ios/LabSoundBridge_ios.tar.gz -C ./ios/build-xcframework LabSoundBridge.xcframework
+echo "*** lab_sound_bridge for iOS built"
